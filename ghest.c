dc0f687a782433027f24011430fa9804d571b7ab
csku25 <102607676+csku25@users.noreply.github.com>
2024-04-16 16:44:09 -0400

    Update ghest.c

--- ghest.c
+++ ghest.c
@@ -1,16 +1,16 @@
 /*
 Filename: ghest.c
 */
-#include <pico/stdlib.h>
-#include <pico/cyw43_arch.h>
-#include <btstack_run_loop.h>
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "btstack.h"
 
 int main() {
-    stdio_init_all();
-    if (cyw43_arch_init()) {
-        printf("Wireless init failed");
+    stdio_init_all(); //needed for everything
+    if (cyw43_arch_init()) { //specifically for Pico W
+        printf("Error: Failed to intialize cyw43_arch!\n");
         return -1;
     }
-    btstack_main(0, NULL);
-    btstack_run_loop_execute();
 }
